e2dec561c8e398658cf45d428bd0e7f442bff0a7
Sanjugowda98 <sanjugowdah604@gmail.com>
2026-02-05 20:45:20 +0530

    checking if 10 is even or not using num using class

--- learning_git.py
+++ learning_git.py
@@ -1,7 +1,10 @@
+
+class Evenodd:
 def even_odd(start,end):
     for num in range(start,end):
         if num%2==0:
             print(f"{num}is even")
         else:
             print(f"{num}is odd")
-even_odd(10,20)
+obj=Evenodd()
+obj.even_odd(10,20)
