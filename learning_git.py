c81d70ffa310cf0b4bc446e2a916089d9241306e
Sanjugowda98 <sanjugowdah604@gmail.com>
2026-02-06 19:59:57 +0530

    class indentation even or odd

--- learning_git.py
+++ learning_git.py
@@ -1,10 +1,10 @@
 
 class Evenodd:
-def even_odd(start,end):
-    for num in range(start,end):
-        if num%2==0:
-            print(f"{num}is even")
-        else:
-            print(f"{num}is odd")
+    def even_odd(self,start,end):
+        for num in range(start,end):
+            if num%2==0:
+                print(f"{num}is even")
+            else:
+                print(f"{num}is odd")
 obj=Evenodd()
 obj.even_odd(10,20)
